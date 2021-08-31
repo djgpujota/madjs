8520d23ebac51fee406cd1f78bd53c3bf9cc4e33
Madeleine Urresta <mgj.urresta@yavirac.edu.ec>
2021-08-31 15:12:02 -0500

    validacioneslistas

--- modules/database.py
+++ modules/database.py
@@ -1,20 +1,22 @@
 import psycopg2
 
 
-try:
-            con= psycopg2.connect(
-                host = 'localhost',
-                database = 'prueba',
-                user = 'postgres',
-                password  = '1234'
-                )
 
-except:
-            print('Error al conectarse ')    
+def abrirConexion():
+    try:
+        con= psycopg2.connect(
+            host = 'localhost',
+            database = 'Proyecto',
+            user = 'postgres',
+            password  = 'Negruras123.'
+            )
+        return con
 
-def cerrarConexion():
+    except:
+        print('Error al conectarse ')    
+
+def cerrarConexion(con):
     con.close()
-
 
 
 
--- routes.py
+++ routes.py
@@ -1,14 +1,15 @@
 from flask import Flask 
-from flask import render_template,request,redirect
+from flask import render_template,request,redirect, flash, url_for
 from datetime import datetime
 from random import randint
+from modules.users import users, Persona
 
 
 
 
 
 app=Flask(__name__)
-
+app.secret_key="Develoteca"
 # index 
 @app.route('/')
 def index():
@@ -17,12 +18,53 @@ def index():
 
 # users 
 @app.route('/users/login')
-def loguin():
+def login():
+   users.admin()
    return render_template('users/login.html')
 
-@app.route('/users/registrar')
+@app.route('/users/login_admin', methods=['GET', 'POST'])
+def loguin():
+   if request.method == 'POST':
+      email=request.form['email']
+      password= request.form['password']
+      print(email,password)
+      correcto = users.loguin(email, password)
+      if (correcto==0):
+         flash ("Usuario y contraseña no validos")
+         return redirect(url_for('login'))
+      if (correcto==1):
+         if(email == 'MADJS@gmail.com'):
+            return redirect(url_for('panel'))
+
+         return redirect(url_for('index'))
+
+
+@app.route('/users/registrar/', methods=['GET','POST'])
 def registrar():
-   return render_template('users/registrar.html')
+      form=Persona()
+
+      if form.validate_on_submit():
+         try:
+            name=request.form['nombre']
+            dir=request.form['direccion']
+            telf=int(request.form['telefono'])
+            email=request.form['correo']
+            clave1=request.form['clave']
+            print(type(name),dir,telf,email,clave1)
+            
+            if telf<0:
+               flash('Error en el campo telefono')
+               return redirect(url_for('registrar'))
+
+            
+            users.registro(name,dir,telf,email,clave1)
+            return redirect('/users/registrar/')
+         except ValueError:
+            # Esta captura el error cuando se manda un string en el telefono
+            flash('Esta ingresando datos erroneos')
+            return redirect(url_for('registrar'))
+
+      return render_template('users/registrar.html', form=form)
 
 
 # admin
